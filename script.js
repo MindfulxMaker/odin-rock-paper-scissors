bfd88860d36d5b047353646ee981f208690e44df
Navic V. <navic.vo@gmail.com>
2024-12-27 21:23:06 +0700

    Finished the function to play 1 round

--- script.js
+++ script.js
@@ -17,10 +17,34 @@ function getComputerChoice() {
 function getHumanChoice() {
     const result = prompt('What is your choice?').toLowerCase();
     console.log(result);
+    return result;
 }
 
 function playRound(humanChoice, computerChoice) {
-
+    if (humanChoice === computerChoice) {
+        console.log("It's a tie!");
+    } else if ((humanChoice === 'paper') && (computerChoice === 'rock')) {
+        humanScore++;
+        console.log(`You win! Your score: ${humanScore}, Computer score: ${computerScore}`);
+    } else if ((humanChoice === 'rock') && (computerChoice === 'scissors')) {
+        humanScore++;
+        console.log(`You win! Your score: ${humanScore}, Computer score: ${computerScore}`);
+    } else if ((humanChoice === 'scissors') && (computerChoice === 'paper')) {
+        humanScore++;
+        console.log(`You win! Your score: ${humanScore}, Computer score: ${computerScore}`);
+    } else if ((computerChoice === 'paper') && (humanChoice === 'rock')) {
+        computerScore++;
+        console.log(`You lose! Your score: ${humanScore}, Computer score: ${computerScore}`);
+    } else if ((computerChoice === 'rock') && (humanChoice === 'scissors')) {
+        computerScore++;
+        console.log(`You lose! Your score: ${humanScore}, Computer score: ${computerScore}`);
+    } else if ((computerChoice === 'scissors') && (humanChoice === 'paper')) {
+        computerScore++;
+        console.log(`You lose! Your score: ${humanScore}, Computer score: ${computerScore}`);
+    }
 }
-getComputerChoice();
-getHumanChoice();
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
