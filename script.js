06955122058de522604667669abe11f7f14d9ac4
Navic V. <navic.vo@gmail.com>
2024-12-27 21:03:23 +0700

    Add functions for computer and human Computer: random options Human: input using promt

---
+++ script.js
@@ -0,0 +1,18 @@
+function getComputerChoice() {
+    const random = Math.floor(Math.random() * 3);
+    let result = "";
+    (random === 1)
+        ? result = "rock"
+        : (random === 2)
+            ? result = "paper"
+            : result = "scissors"
+    console.log(result);
+    return result;
+}
+
+function getHumanChoice() {
+    const result = prompt('What is your choice?');
+    console.log(result);
+}
+getComputerChoice();
+getHumanChoice();
